d648954c79aa87020485c1a19463830315566c2f
LieneLittlebird <liene.putnina20@gmail.com>
2021-03-23 22:31:40 +0200

    Function for Fibonacci in a sequence done

--- labB.cpp
+++ labB.cpp
@@ -1,9 +1,10 @@
 /* Liene Putniņa, lr12022
-B12. Given natural number n. Find the Fibonachi number f(n). 
+B12. Given a natural number n. Find the Fibonachi number f(n). 
 Create and use a function to calculate the Fibonachi number f(n). 
 (Fibonachi numbers are f(1) = 1, f(2) = 1, f(n) = f(n-1) + f(n-2), t. i. f(3)=2, f(4)=3, f(5)=5 utt.). 
 Also, check if the given number n is a Fibonachi number. 
 Create and use a function for this check.
+Program created at: XXXX */
 
 /* 1.Error message for incorrect input
 2. Repeated execution
@@ -17,14 +18,25 @@ using namespace std;
 
 int fibonacciNumbers()
 {
+    int count = 0;
     int inputNumber;
     int fiboNum;
-    cout << "Enter a few numbers: \n";
-    cin >> inputNumber;
-    // Each number is the sum of the two preceding ones
-    fiboNum = (inputNumber - 1) + (inputNumber - 2);
 
-    return fiboNum;
+    cout << "Enter the number of elements: \n";
+    cin >> count;
+    cout << "Enter " << count << " numbers: \n";
+
+    for (int i = 0; i < count; i++)
+    {
+        cin >> inputNumber;
+
+        if (inputNumber == (inputNumber - 1) + (inputNumber - 2))
+        {
+            cout << "Previous number - a Fibonacci number" << endl;
+        }
+    }
+
+    return 0;
 }
 
 // Execution of the program
